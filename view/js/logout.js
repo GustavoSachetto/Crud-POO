9f2b6ce4ad93825d505609aa5f9b5ab1979520aa
Gustavo Sachetto <g.sachettocruz@gmail.com>
2024-11-02 17:05:28 -0300

    remove: console.log utilizado para teste

--- view/js/logout.js
+++ view/js/logout.js
@@ -2,8 +2,6 @@ const button = document.querySelector('#logout');
 
 if (button) {
     button.addEventListener('click', () => {
-        console.log(baseUrl+'/view/page/logout.php');
-        
         window.location.replace(baseUrl+'/view/page/logout.php');
     });
 }
